1c15b002768e3f1388e5034848c70518815de707
leichtokari <leicht@okari.com>
2026-04-21 12:20:49 +0200

    Update scraper_multi.py

--- scraper_multi.py
+++ scraper_multi.py
@@ -5,9 +5,10 @@ from ai_model import classify_tender
 
 OUTPUT_EXCEL = "tenders.xlsx"
 
-###############
+
+########################
 # 1) Bayern
-###############
+########################
 def scrape_bayern():
     url = "https://www.auftraege.bayern.de"
     try:
@@ -18,6 +19,7 @@ def scrape_bayern():
         for item in soup.select(".result-list-item"):
             title = item.select_one(".result-title")
             link = item.select_one("a")
+
             if not title or not link:
                 continue
 
@@ -30,9 +32,10 @@ def scrape_bayern():
     except:
         return []
 
-######################
-# 2) NetServer family
-######################
+
+##############################
+# 2) NetServer (shared logic)
+##############################
 def scrape_netserver(url, site_name):
     try:
         r = requests.get(url, timeout=12)
@@ -42,6 +45,7 @@ def scrape_netserver(url, site_name):
         for item in soup.select(".nl-list-item, .result-list-item"):
             title = item.select_one(".nl-title, .result-title")
             link = item.select_one("a")
+
             if not title or not link:
                 continue
 
@@ -54,8 +58,9 @@ def scrape_netserver(url, site_name):
     except:
         return []
 
+
 ###########################
-# 3) DTvP – Deutsches Vergabeportal
+# 3) DTVP Portal
 ###########################
 def scrape_dtvp():
     url = "https://www.dtvp.de/Center/public/projectsearch"
@@ -67,8 +72,10 @@ def scrape_dtvp():
         for item in soup.select(".project-result"):
             title = item.select_one(".project-title")
             link = item.select_one("a")
+
             if not title or not link:
                 continue
+
             tenders.append({
                 "site": "DTVP",
                 "title": title.get_text(strip=True),
@@ -78,9 +85,10 @@ def scrape_dtvp():
     except:
         return []
 
-###################
+
+###############################
 # 4) eVergabe Online
-###################
+###############################
 def scrape_evergabe():
     url = "https://www.evergabe-online.de/oeffentliche-ausschreibungen"
     try:
@@ -91,8 +99,10 @@ def scrape_evergabe():
         for item in soup.select(".search-result-item"):
             title = item.select_one(".title")
             link = item.select_one("a")
+
             if not title or not link:
                 continue
+
             tenders.append({
                 "site": "eVergabe",
                 "title": title.get_text(strip=True),
@@ -102,20 +112,89 @@ def scrape_evergabe():
     except:
         return []
 
-##################
+
+###############################
 # 5) Hessen HAD
-##################
+###############################
 def scrape_had():
     url = "https://www.had.de"
     try:
         r = requests.get(url, timeout=10)
         soup = BeautifulSoup(r.text, "html.parser")
+
         tenders = []
-        for item in soup.select("table tr"):
-            cols = item.select("td")
+        for row in soup.select("table tr"):
+            cols = row.select("td")
             if len(cols) > 1:
                 title = cols[1].get_text(strip=True)
                 tenders.append({
                     "site": "HAD Hessen",
                     "title": title,
-    
+                    "link": url
+                })
+        return tenders
+    except:
+        return []
+
+
+###############################
+# 6) EU TED (API)
+###############################
+def scrape_ted():
+    api_url = "https://ted.europa.eu/api/v2/notices/search?fields=TITLE"
+    try:
+        r = requests.get(api_url, timeout=12)
+        data = r.json()
+
+        tenders = []
+        for item in data.get("results", []):
+            title = item.get("title", [""])[0]
+            tenders.append({
+                "site": "TED EU",
+                "title": title,
+                "link": "https://ted.europa.eu"
+            })
+        return tenders
+    except:
+        return []
+
+
+###############################
+# 7) MAIN SCRAPER
+###############################
+async def run_scraper():
+    all_tenders = []
+
+    # Bayern
+    all_tenders += scrape_bayern()
+
+    # NetServer group
+    netservers = [
+        ("https://xvergabe.de/NetServer/PublicationSearchControllerServlet?Max=50&Category=InvitationToTender&Gesetzesgrundlage=VOL&function=SearchPublications&thContext=publications", "xVergabe"),
+        ("https://vergabe.muenchen.de/NetServer", "Munich"),
+        ("https://vergabe.autobahn.de/NetServer/PublicationSearchControllerServlet?function=SearchPublications&Gesetzesgrundlage=All&Category=InvitationToTender&thContext=publications", "Autobahn"),
+        ("https://vergabe.landbw.de/NetServer", "LandBW"),
+        ("https://www.vergabe.rlp.de/VMPCenter/company/welcome.do", "RLP")
+    ]
+    for url, name in netservers:
+        all_tenders += scrape_netserver(url, name)
+
+    # DTVP
+    all_tenders += scrape_dtvp()
+
+    # eVergabe Online
+    all_tenders += scrape_evergabe()
+
+    # Hessen HAD
+    all_tenders += scrape_had()
+
+    # TED EU
+    all_tenders += scrape_ted()
+
+    # AI Filter
+    relevant = [t for t in all_tenders if classify_tender(t["title"])]
+
+    df = pd.DataFrame(relevant)
+    df.to_excel(OUTPUT_EXCEL, index=False)
+
+    return df
